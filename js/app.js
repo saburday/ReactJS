82475809b2851f84cd2eacc00b174b88dd1c6ea1
saburday <yteslenko@yahoo.com>
2017-09-04 15:57:49 +0300

    next propTypes

---
+++ js/app.js
@@ -0,0 +1,79 @@
+var my_news = [
+  {
+				author:	'Саша	Печкин',
+				text:	'В	четверг,	четвертого	числа...'
+		},
+		{
+				author:	'Просто	Вася',
+				text:	'Считаю,	что	$	должен	стоить	35	рублей!'
+		},
+		{
+
+				text:	'Бесплатно.	Скачать.	Лучший	сайт	-	http://localhost:3000'
+		}
+];
+
+var Article = React.createClass({
+  propTypes:	{
+  				data:	React.PropTypes.shape({
+  						author:	React.PropTypes.string.isRequired,
+  						text:	React.PropTypes.string.isRequired
+  				})
+  		},
+
+
+  render: function() {
+    var author=this.props.data.author,
+        text = this.props.data.text;
+
+    return(
+      <div className="article">
+       <p className="news__author">{author}:</p>
+       <p className="news__text">{text}</p>
+      </div>
+    )
+  }
+});
+
+var News = React.createClass({
+  render:function() {
+    var data = this.props.data;
+    var newsTemplate;
+
+    if (data.length > 0) {
+    newsTemplate = data.map(function(item, id){
+      return (
+        <div key = {id}>
+        <Article data ={item} />
+        </div>
+      )
+    })
+  } else {
+    newsTemplate = <p>К сожалению новостей нет</p>
+  }
+
+  return (
+      <div className = "news">
+        {newsTemplate}
+        <strong className ={'news__count '+(data.length > 0 ? '':'none') }>Всего новостей: {data.length}</strong>
+      </div>
+    );
+  }
+});
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div className="app">
+        <h3>Новости</h3>
+        <News data={my_news}/>
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <App />,
+
+  document.getElementById('root')
+);
